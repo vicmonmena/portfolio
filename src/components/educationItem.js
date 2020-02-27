e93384e82ce7ec0c372d2929500dfa9cd2f71f98
vmontanomena <vicente.montanomena@telefonica.com>
2020-02-27 23:46:41 +0100

    STYLES: Maquetando educationItem

--- src/components/educationItem.js
+++ src/components/educationItem.js
@@ -2,15 +2,16 @@ import React from "react"
 import PropTypes from "prop-types"
 import "./educationItem.css"
 
-const EducationItem = ({logo, title, description, period}) => {
+const EducationItem = ({logo, titulo, description, period, centro}) => {
 
   return (
     <div className ='education-item'>
       <div className ='education-item-logo'>{logo}</div>
       <div className ='education-item-info'>
-        <div className = 'education-item-info-top'>
-          <div className ='education-item-title'>{title}</div><div className='education-item-period'>{period}</div>
+        <div className = 'education-item-titulo'>
+          <div className ='education-item-title'>{titulo}</div><div className='education-item-period'>{period}</div>
         </div>
+        <div className ='education-item-centro'>{centro}</div>
         <div className ='education-item-description'>
           {description}
         </div>
@@ -21,9 +22,10 @@ const EducationItem = ({logo, title, description, period}) => {
 
 EducationItem.propTypes = {
   logo: PropTypes.string,
-  title: PropTypes.string,
+  titulo: PropTypes.string,
   description: PropTypes.string,
-  period: PropTypes.string
+  period: PropTypes.string,
+  centro: PropTypes.string
 }
 
 export default EducationItem
