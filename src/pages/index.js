a98ab09880acd3cc644752106642ff31eb398581
vmontanomena <vicente.montanomena@telefonica.com>
2020-02-27 23:47:18 +0100

    FEAT: Implementano prop location como requerida

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import PropTypes from "prop-types"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -9,5 +9,9 @@ const IndexPage = ({location}) => (
     <h1>{location.pathname}</h1>
   </Layout>
 )
+
+IndexPage.propTypes = {
+  location: PropTypes.object.isRequired,
+}
 
 export default IndexPage
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,13 +1,17 @@
 import React from "react"
-
+import PropTypes from "prop-types"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = ({location}) => (
+const Skills = ({location}) => (
   <Layout path={location.pathname}>
     <SEO title="Home" />
     <h1>{location.pathname}</h1>
   </Layout>
 )
 
-export default IndexPage
+Skills.propTypes = {
+  location: PropTypes.object.isRequired,
+}
+
+export default Skills
