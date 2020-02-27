4827ae4d59db1063121839ef99b044b133e8e332
vmontanomena <vicente.montanomena@telefonica.com>
2020-02-27 23:47:44 +0100

    FEAT: Cargando datos del json con la info de educación (graphql)

--- src/pages/education.js
+++ src/pages/education.js
@@ -8,44 +8,79 @@ import "./pages.css"
 
 function Education({location}) {
 
-  const { allEducationItemsJson } = useStaticQuery(
-    graphql`
-      query {
-        allEducationItemsJson {
-          edges {
-            node {
-              logo
-              centro
-              titulo
-              descripcion
-              periodo
-            }
+  const data = useStaticQuery(graphql`
+  {
+    allDataJson {
+      nodes {
+        no_reglada {
+          centro
+          id
+          description
+          logo
+          periodo {
+            desde
+            hasta
           }
+          titulo
+        }
+        reglada {
+          centro
+          descripcion
+          id
+          logo
+          periodo {
+            desde
+            hasta
+          }
+          titulo
         }
       }
-    `
-  )
-
+    }
+  }`)
+  console.log('data: ', data);
+    
   return (
     <Layout path={location.pathname}>
       <SEO title="Home" />
       <h1>{location.pathname}</h1>
       <div className ='education-container'>
-        {allEducationItemsJson.edges.forEach(item => (
-          <EducationItem 
-            logo={item.logo}
-            title={item.titulo}
-            date={item.periodo}
-            description={item.descripcion}
-          />
-        ))}
-    </div>
+        <div className ='education-container-reglada'>
+          {
+            data.allDataJson.nodes[0].reglada.map(item => {
+              return (
+                <EducationItem 
+                  logo={item.logo}
+                  titulo={item.titulo}
+                  date={item.periodo}
+                  description={item.descripcion}
+                  centro={item.centro}
+                />
+              )
+            })
+          }
+        </div>
+        <div className ='education-container-noreglada'>
+          {
+            data.allDataJson.nodes[0].no-reglada.map(item => {
+              return (
+                <EducationItem 
+                  logo={item.logo}
+                  titulo={item.titulo}
+                  date={item.periodo}
+                  description={item.descripcion}
+                  centro={item.centro}
+                />
+              )
+            })
+          }
+        </div>
+      </div>
     </Layout>
   )
 }
 
 Education.propTypes = {
-  location: PropTypes.string,
+  location: PropTypes.object.isRequired,
 }
 
 export default Education
