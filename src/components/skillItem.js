445a1aad65b008b6bc74ee2a222738417acd8061
vmontanomena <vicente.montanomena@telefonica.com>
2020-03-02 22:17:22 +0100

    FEAT: Implementando componente para mostrar una skill

---
+++ src/components/skillItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const skillsItem = ({title, summary, handleClick}) => {
+  // TODO: crear tooltip con el summary
+  // TODO: capturar evento handleClick para mostrar fuera una descripción de la skill
+  return (<div>{title}</div>)
+}
+
+export default skillsItem
