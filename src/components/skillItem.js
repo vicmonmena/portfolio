d5a48e43be9ed7e7d1d8ae4c31f9e2650885bdee
vmontanomena <vicente.montanomena@telefonica.com>
2020-03-03 22:47:40 +0100

    STYLES: Maquetando seccións skills

--- src/components/skillItem.js
+++ src/components/skillItem.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Tippy from '@tippy.js/react';
+import 'tippy.js/dist/tippy.css';
+import "./skillItem.css"
+ 
 
 const skillsItem = ({title, summary, handleClick}) => {
   // TODO: crear tooltip con el summary
   // TODO: capturar evento handleClick para mostrar fuera una descripción de la skill
-  return (<div>{title}</div>)
+  return (
+    <div className="skill-item">
+      <Tippy content={<span>{summary}</span>}>
+        <button onClick={handleClick}>{title}</button>
+      </Tippy>
+    </div>
+  )
 }
 
 export default skillsItem
