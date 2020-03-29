e3ded34c52705b67b69fbdd4c4d08f22c9aa2077
vicmonmena <vicmonena@gmail.com>
2020-03-29 19:44:24 +0200

    INFO: Complementando la info de la experiencia

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -38,6 +38,70 @@ const Timeline = () => {
         since="Enero 2015"
         to="Junio 2016"
       />
+      <ExperienceItem
+        itemType="work"
+        title="Software Developer"
+        subtitle="SOLTEL"
+        description="Encargado de realizar tareas de desarrollo e integración con software: Liferay + JSF + Java + HTML + CSS + Alfresco"
+        since="Noviembre 2014"
+        to="Diciembre 2014"
+      />
+      <ExperienceItem
+        itemType="work"
+        title="Software Developer"
+        subtitle="Abengoa (Simosa IT)"
+        description="Encargado de tareas de soporte para software BPM Bizagi"
+        since="Junio 2013"
+        to="Noviembre 2014"
+      />
+      <ExperienceItem
+        itemType="work"
+        title="Software Developer"
+        subtitle="SOLTEL"
+        description="Trabajé realizando tareas de desarrollo con Drupal, JEE, Android, estimación, análisis y demos a clientes."
+        since="Marzo 2013"
+        to="Junio 2013"
+      />
+      <ExperienceItem
+        itemType="work"
+        title="Web and Mobile Developer"
+        subtitle="Abengoa (Simosa IT)"
+        description="Realicé tareas de desarrollo y correcciones sobre aplicaciones Web JEE y app para Blackberry"
+        since="Agosto 2012"
+        to="Marzo 2013"
+      />
+      <ExperienceItem
+        itemType="work"
+        title="Software Developer"
+        subtitle="SOLTEL"
+        description="Desarrollo web con Bonita BPM"
+        since="Febrero 2012"
+        to="Agosto 2012"
+      />
+      <ExperienceItem
+        itemType="work"
+        title="Android Developer"
+        subtitle="SOLTEL"
+        description="Desarrollo de app GlutenFreeList para dispositivos Android"
+        since="Septiembre 2011"
+        to="Febrero 2012"
+      />
+      <ExperienceItem
+        itemType="work"
+        title="Junior Software Developer"
+        subtitle="SOLTEL"
+        description="Trabajé realizando tareas de desarrollo web (J2EE, Struts2, Ibernate, OpenCMS)."
+        since="Julio 2009"
+        to="Septiembre 2011"
+      />
+      <ExperienceItem
+        itemType="work"
+        title="Programador en Prácticas"
+        subtitle="SOLTEL"
+        description="Desarrollador web para plataformas J2EE y .NET Web application development based on JEE platform and .NET framework"
+        since="Junio 2009"
+        to="Julio 2009"
+      />
       <ExperienceItem itemType="star" />
     </VerticalTimeline>
   );
