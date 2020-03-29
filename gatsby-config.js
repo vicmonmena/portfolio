4771375e15d3a08c9467507af9b4b2ff487c02c3
vicmonmena <vicmonena@gmail.com>
2020-03-29 23:09:16 +0200

    FEAT: Actualizando metadata de la web

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Portfolio`,
+    description: `Porfolio of Vicente Montaño Mena`,
+    author: `@vicmonmena <vicmonmena@gmail.com>`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/mi_foto.jpg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-material-ui`, 
